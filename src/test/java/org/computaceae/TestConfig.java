8d4c605f10906b46a63258bc0d19e7e272453fda
SiropOps <cyril.boillat@ville-ge.ch>
2020-07-03 13:04:24 +0200

    feat (CONFIF) add test config

--- src/test/java/org/computaceae/TestConfig.java
+++ src/test/java/org/computaceae/TestConfig.java
@@ -1,8 +1,10 @@
 package org.computaceae;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Profile;
 
 @TestConfiguration
+@Profile({"test"})
 public class TestConfig {
 
 }
