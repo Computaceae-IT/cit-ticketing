395ed4cf50109a28fa2d9a89d6c665737f82ab61
SiropOps <cyril.boillat@ville-ge.ch>
2020-07-13 17:40:47 +0200

    feat (TEST) added

--- src/test/java/org/computaceae/ticketing/integration/UserServiceIntegrationTest.java
+++ src/test/java/org/computaceae/ticketing/integration/UserServiceIntegrationTest.java
@@ -1,131 +1,120 @@
-//package org.computaceae.ticketing.integration;
-//
-//import static org.junit.Assert.assertTrue;
-//import org.computaceae.TestConfig;
-//import org.computaceae.ticketing.service.TicketingService;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import com.lib.cit.core.dto.ticketing.TicketDTO;
-//import com.lib.cit.core.errors.container.value.InconsistentEmptyValue;
-//import com.lib.cit.core.errors.exception.LogicalBusinessException;
-//
-//@ActiveProfiles("test")
-//@Import(TestConfig.class)
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = WebEnvironment.NONE)
-//public class UserServiceIntegrationTest {
-//
-//  private static Logger log = LoggerFactory.getLogger(UserServiceIntegrationTest.class);
-//
-//  @Autowired
-//  private TicketingService ticketingService;
-//
-//  @Test
-//  public void createWithEmptyValueTest() {
-//    try {
-//      this.ticketingService.create(null);
-//      throw new AssertionError("No logicalBusinessException raised for create with null object");
-//    } catch (LogicalBusinessException e) {
-//      assertTrue(e.toString(), e.getListError() != null);
-//      assertTrue(e.toString(), e.getListError().size() == 1);
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0) instanceof InconsistentEmptyValue);
-//    } catch (Exception e) {
-//      log.error(e.getMessage(), e);
-//      throw new AssertionError(e);
-//    }
-//
-//    /*** EMPTY PROPERTIES ***/
-//    TicketDTO ticket = new TicketDTO();
-//
-//    try {
-//      this.ticketingService.create(ticket);
-//      throw new AssertionError("No logicalBusinessException raised for create with null object");
-//    } catch (LogicalBusinessException e) {
-//      assertTrue(e.toString(), e.getListError() != null);
-//      assertTrue(e.toString(), e.getListError().size() == 3);
-//      // Title empty
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0) instanceof InconsistentEmptyValue);
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0).getKey().equals("ticket.title"));
-//      // label empty
-//      assertTrue(e.getListError().get(1).toString(),
-//          e.getListError().get(1) instanceof InconsistentEmptyValue);
-//      assertTrue(e.getListError().get(1).toString(),
-//          e.getListError().get(1).getKey().equals("ticket.label"));
-//      // label url
-//      assertTrue(e.getListError().get(2).toString(),
-//          e.getListError().get(2) instanceof InconsistentEmptyValue);
-//      assertTrue(e.getListError().get(2).toString(),
-//          e.getListError().get(2).getKey().equals("ticket.url"));
-//
-//    } catch (Exception e) {
-//      log.error(e.getMessage(), e);
-//      throw new AssertionError(e);
-//    }
-//
-//    /*** ONLY TITLE ***/
-//    ticket.setTitle("MOCK TITLE");
-//
-//    try {
-//      this.ticketingService.create(ticket);
-//      throw new AssertionError("No logicalBusinessException raised for create with null object");
-//    } catch (LogicalBusinessException e) {
-//      assertTrue(e.toString(), e.getListError() != null);
-//      assertTrue(e.toString(), e.getListError().size() == 2);
-//
-//      // label empty
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0) instanceof InconsistentEmptyValue);
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0).getKey().equals("ticket.label"));
-//
-//      // label url
-//      assertTrue(e.getListError().get(1).toString(),
-//          e.getListError().get(1) instanceof InconsistentEmptyValue);
-//      assertTrue(e.getListError().get(1).toString(),
-//          e.getListError().get(1).getKey().equals("ticket.url"));
-//
-//    } catch (Exception e) {
-//      log.error(e.getMessage(), e);
-//      throw new AssertionError(e);
-//    }
-//  }
-//
-//  @Test
-//  public void createWithWrongLabelTest() {
-//
-//    TicketDTO ticket = new TicketDTO();
-//    ticket.setTitle("MOCK TITLE");
-//    ticket.setLabel("FAKE_MOCK_LABEL");
-//    ticket.setUrl("MOCK_URL");
-//
-//    try {
-//      this.ticketingService.create(ticket);
-//      throw new AssertionError("No logicalBusinessException raised for create with null object");
-//    } catch (LogicalBusinessException e) {
-//      assertTrue(e.getListError() != null);
-//      assertTrue(e.toString(), e.getListError().size() == 1);
-//      // label empty
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0) instanceof InconsistentEmptyValue);
-//      assertTrue(e.getListError().get(0).toString(),
-//          e.getListError().get(0).getKey().equals("ticket.label"));
-//
-//    } catch (Exception e) {
-//      log.error(e.getMessage(), e);
-//      throw new AssertionError(e);
-//    }
-//
-//  }
-//
-//}
+package org.computaceae.ticketing.integration;
+
+import static org.junit.Assert.assertTrue;
+import java.util.HashMap;
+import org.computaceae.TestConfig;
+import org.computaceae.ticketing.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("test")
+@Import(TestConfig.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class UserServiceIntegrationTest {
+
+  private static Logger log = LoggerFactory.getLogger(UserServiceIntegrationTest.class);
+
+  @Autowired
+  private UserService userService;
+
+  @Value("${app.admin.mail}")
+  private String defaultMail;
+
+  @Test
+  public void addMailUserWithEmptyValueTest() {
+
+    try {
+      this.userService.addMailUser(null);
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      throw new AssertionError(e);
+    }
+
+    try {
+      this.userService.addMailUser(new HashMap<String, String>());
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      throw new AssertionError(e);
+    }
+
+
+    try {
+      this.userService.addMailUser(new HashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        {
+          put(null, "MOCK");
+        }
+      });
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      throw new AssertionError(e);
+    }
+
+    try {
+      this.userService.addMailUser(new HashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        {
+          put("MOCK", null);
+        }
+      });
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      throw new AssertionError(e);
+    }
+
+    assertTrue(this.userService.getEmail("MOCK") + ":" + this.defaultMail,
+        this.userService.getEmail("MOCK").contains(this.defaultMail));
+
+  }
+
+  @Test
+  public void addMailUserTest() {
+
+
+    try {
+      this.userService.addMailUser(new HashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        {
+          put("MOCK1", "MOCK1");
+        }
+      });
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      throw new AssertionError(e);
+    }
+
+    try {
+      this.userService.addMailUser(new HashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        {
+          put("MOCK2", "MOCK2");
+        }
+      });
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      throw new AssertionError(e);
+    }
+
+    assertTrue(this.userService.getEmail("MOCK1"),
+        this.userService.getEmail("MOCK1").equals("MOCK1 <MOCK1>"));
+
+    assertTrue(this.userService.getEmail("MOCK2"),
+        this.userService.getEmail("MOCK2").equals("MOCK2 <MOCK2>"));
+
+    assertTrue(this.userService.getEmail("MOCK3"),
+        this.userService.getEmail("MOCK3").equals("MOCK3 <" + this.defaultMail + ">"));
+
+  }
+
+
+}
