b1b4d3f0e792cde4dc9884c411ba8be90b865fc1
SiropOps <cyril.boillat@ville-ge.ch>
2020-07-15 14:19:29 +0200

    feat (JAVADOC)

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.computaceae.ticketing.controller"},{"l":"org.computaceae.ticketing.service"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.computaceae.ticketing.controller"}]
