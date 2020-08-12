4958b8ab98d52f7081419a16148862c807ba8090
SiropOps <cyril.boillat@ville-ge.ch>
2020-08-12 07:54:02 +0200

    fix. (CRON) change days delta

--- src/main/java/org/computaceae/ticketing/service/TicketingServiceImpl.java
+++ src/main/java/org/computaceae/ticketing/service/TicketingServiceImpl.java
@@ -116,8 +116,8 @@ public class TicketingServiceImpl implements TicketingService {
    * 
    */
   @Override
-  @Scheduled(cron = "0 2 0 * * *")
-  // At 00:02.
+  @Scheduled(cron = "0 2 22 * * *")
+  // At 22:02.
   public void getLastUpdatedIssues() {
     LocalDate now = LocalDate.now();
     LocalDate updatedAt;
