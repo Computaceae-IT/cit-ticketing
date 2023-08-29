7b7b635dc97559169593519c12ecb0a1c167cede
SiropOps <cyril@botalista.community>
2023-08-29 05:48:35 +0200

    fix (TEST)

--- src/main/java/org/computaceae/ticketing/component/DataRefreshComponent.java
+++ src/main/java/org/computaceae/ticketing/component/DataRefreshComponent.java
@@ -2,7 +2,6 @@ package org.computaceae.ticketing.component;
 
 import org.computaceae.lib.core.message.TicketingMessage;
 import org.computaceae.ticketing.config.RabbitDispatcherConfig;
-import org.computaceae.ticketing.service.TicketingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +16,6 @@ public class DataRefreshComponent {
   @Autowired
   private RabbitDispatcherConfig rabbitDispatcherConfig;
 
-  @Autowired
-  private TicketingService ticketingService;
-
   @Async
   public synchronized void refreshUsersOnStartup() {
 
@@ -30,15 +26,6 @@ public class DataRefreshComponent {
       Thread.currentThread().interrupt();
     }
     this.rabbitDispatcherConfig.sendTicketing(new TicketingMessage(true));
-
-
-    try {
-      this.wait(120000); // 2 minutes
-    } catch (InterruptedException ex) {
-      log.error(ex.getMessage(), ex);
-      Thread.currentThread().interrupt();
-    }
-    this.ticketingService.getLastUpdatedIssues();
   }
 
 }
