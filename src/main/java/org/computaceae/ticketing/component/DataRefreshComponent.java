1879601976e391ea2958111ef7336bc14623a93e
SiropOps <cyril@botalista.community>
2023-08-29 05:35:27 +0200

    feat. (K8S) improve config

--- src/main/java/org/computaceae/ticketing/component/DataRefreshComponent.java
+++ src/main/java/org/computaceae/ticketing/component/DataRefreshComponent.java
@@ -2,6 +2,7 @@ package org.computaceae.ticketing.component;
 
 import org.computaceae.lib.core.message.TicketingMessage;
 import org.computaceae.ticketing.config.RabbitDispatcherConfig;
+import org.computaceae.ticketing.service.TicketingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ public class DataRefreshComponent {
   @Autowired
   private RabbitDispatcherConfig rabbitDispatcherConfig;
 
+  @Autowired
+  private TicketingService ticketingService;
+
   @Async
   public synchronized void refreshUsersOnStartup() {
 
@@ -26,6 +30,15 @@ public class DataRefreshComponent {
       Thread.currentThread().interrupt();
     }
     this.rabbitDispatcherConfig.sendTicketing(new TicketingMessage(true));
+
+
+    try {
+      this.wait(120000); // 2 minutes
+    } catch (InterruptedException ex) {
+      log.error(ex.getMessage(), ex);
+      Thread.currentThread().interrupt();
+    }
+    this.ticketingService.getLastUpdatedIssues();
   }
 
 }
